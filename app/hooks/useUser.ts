dfeea40153f2ee5b5cd63f8fdf0adea2bc4d5f41
songjinhoon <jinhoon0319@gmail.com>
2023-11-13 15:38:14 +0900

    일부 수정

--- app/hooks/useUser.ts
+++ app/hooks/useUser.ts
@@ -1,10 +1,12 @@
 import { useCallback } from 'react';
 import { ISignInUser, ISignUpUser } from '@/app/types/user';
 import Api from '@/app/utils/axiosConfig';
+import { useRouter } from 'next/navigation';
 
 const useUser = () => {
+  const router = useRouter();
+
   const signUp = useCallback(async (params: ISignUpUser) => {
-    console.log('debug2');
     try {
       const response = await Api.post(`/api/users/sign-up`, params, {
         withCredentials: true,
@@ -19,7 +21,21 @@ const useUser = () => {
     }
   }, []);
 
-  const signIn = useCallback(async (params: ISignInUser) => {}, []);
+  const signIn = useCallback(async (params: ISignInUser) => {
+    try {
+      const response = await Api.post(`/api/users/sign-in`, {
+        ...params,
+      });
+      if (response.status === 200) {
+        // createAuth(response.data.id);
+        await router.push('/dashboard');
+      } else {
+        new Error('error');
+      }
+    } catch (error: any) {
+      console.error('error');
+    }
+  }, []);
 
   return {
     signUp,
